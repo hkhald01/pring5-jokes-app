a1b0e1e5962b365bdb117c8a4da6ac643f08eebd
heikel <hkhaldi@ford.com>
2019-07-18 11:00:34 -0400

    create a joke service. closes #3

---
+++ src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
@@ -0,0 +1,16 @@
+package guru.springframework.joke.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+    @Autowired
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes){
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
