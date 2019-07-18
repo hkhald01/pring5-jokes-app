ac05dd6d64bdd9831186c1a82d81a060fd6695c2
heikel <hkhaldi@ford.com>
2019-07-18 11:24:59 -0400

    remove autowired and run app. closes #9

--- src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
@@ -1,16 +1,15 @@
 package guru.springframework.joke.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
-    @Autowired
-    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes){
-        this.chuckNorrisQuotes = chuckNorrisQuotes;
+
+    public JokeServiceImpl(){
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
     }
     public String getJoke() {
         return chuckNorrisQuotes.getRandomQuote();
