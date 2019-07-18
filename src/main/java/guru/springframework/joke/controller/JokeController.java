09ae6900aa3b482a6de393197543253f4a00be73
heikel <hkhaldi@ford.com>
2019-07-18 11:09:24 -0400

    return "chucknorris" view. closes #7

--- src/main/java/guru/springframework/joke/controller/JokeController.java
+++ src/main/java/guru/springframework/joke/controller/JokeController.java
@@ -16,7 +16,9 @@ public class JokeController {
     }
     @RequestMapping({"/",""})
     public String showJoke(Model model){
+
         model.addAttribute("joke",jokeService.getJoke());
+
         return "chucknorris";
     }
 }
