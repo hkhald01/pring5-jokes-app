926653bf3e2440fa823dcc369573676d5142364a
heikel <hkhaldi@ford.com>
2019-07-18 11:07:42 -0400

    Map Context root ("/","") to joke view. closes #5

--- src/main/java/guru/springframework/joke/controller/JokeController.java
+++ src/main/java/guru/springframework/joke/controller/JokeController.java
@@ -3,6 +3,8 @@ package guru.springframework.joke.controller;
 import guru.springframework.joke.services.JokeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JokeController {
@@ -12,5 +14,9 @@ public class JokeController {
     public JokeController(JokeService jokeService){
         this.jokeService = jokeService;
     }
-
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke",jokeService.getJoke());
+        return null;
+    }
 }
